d4a7ec3a41fc035a5ad7c54f10bf729189324538
MouseWu <wuhao7171@gmail.com>
2013-08-06 00:16:50 +0800

    上传
    上传功能完善

--- app/controllers/Uploads.java
+++ app/controllers/Uploads.java
@@ -1,7 +1,12 @@
 package controllers;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.google.gson.Gson;
 
 import models.Filename;
 import play.Play;
@@ -15,15 +20,18 @@ public class Uploads extends BaseCore{
 		/* 获取文件哈希码 */
 		int hash = upfile.hashCode();
 		/* 获取文件名字 */
-		String filename = upfile.getName();
+		String allfilename = upfile.getName();
+        /*获取文件除扩展名以外的名字*/
+		String filename = allfilename.substring(0, allfilename.lastIndexOf("."));
 		/* 获取文件扩展名 */
-		String fileext = filename.substring(filename.lastIndexOf("."));
+		String fileext = allfilename.substring(allfilename.lastIndexOf("."));
 		/* 构成hash路径名 */
 		String path = "/public/resourse/" + institute_sel + "/" + subject_sel + "/"
 				+ hash + fileext;
 		Files.copy(upfile, Play.getFile(path));
 		/* 获取当前日期 */
-		Date datenow = new Date();
+		 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		 Date date = new Date();
 		/* 获取上传者的姓名 */
 		String user = session.get("user");
 		/* 将此资源储存到数据库 */
@@ -34,8 +42,24 @@ public class Uploads extends BaseCore{
 		filedatename.subject = subject_sel;
 		filedatename.downcount = 0;
 		filedatename.uploadname = user;
-		filedatename.uploaddate = datenow.toString();
+		filedatename.uploaddate = formatter.format(date);
 		dao.AddFilename.addFile(filedatename);
+		List list = new ArrayList();
+		list.add(filedatename.id);
+		list.add(filedatename.realName);
+		response.contentType = "application/json";
+		response.setHeader("Content-Type", "application/json;charset=UTF-8");
+		Gson gson = new Gson();
+		String listToJson = gson.toJson(list);
+		renderText(listToJson);
+		
+	}
+	/*修改文件名*/
+	public static void alterFilename(String filename,long id){
+	      List<Filename> list = Filename.find("id = ?", id).fetch();
+	      list.get(0).realName = filename;
+	      list.get(0).save();
+		
 	}
 
 }
