9bde2ae51a7073babc089134f90f2a9dfd6dfdf4
MouseWu <wuhao7171@gmail.com>
2013-10-20 02:26:05 +0800

    寻求帮助增加了验证码
    寻求帮助增加了验证码

--- app/controllers/ReSeek.java
+++ app/controllers/ReSeek.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.jaxen.function.StringFunction;
 
+import play.cache.Cache;
 import play.mvc.Before;
 import models.Re_Seek_Help;
 import models.Seek_Help;
@@ -23,23 +24,11 @@ public class ReSeek extends BaseCore {
 			Application.index(tipinfo);
 		}
 	}
-	public static void AddReSeek(String re_seek_content,long seek_id,int nowpage){
+	public static void AddReSeek(String re_seek_content,long seek_id,int nowpage ,String code){
+		if(!code.toUpperCase().equals(((String)Cache.get(""+seek_id)).toUpperCase())){
+			SeekHelp.seek_home(nowpage,(int)(seek_id/1),1);
+		}else{
 		Ask_Tips tips = new Ask_Tips();
-		/*把回复信息加入回复数据表中*/
-//		Re_Seek_Help re_Seek_Help = new Re_Seek_Help();
-//		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//		Date date = new Date();
-//		re_Seek_Help.re_seek_content = re_seek_content;
-//		re_Seek_Help.re_seek_user = session.get("user");
-//		re_Seek_Help.re_seek_date =  formatter.format(date);
-//		Seek_Help seek_Help1 =new Seek_Help();
-//		seek_Help1.id = seek_id;
-//		re_Seek_Help.seek_Help = seek_Help1;
-//		dao.AddResources.addRe_Seek(re_Seek_Help);
-//		List<Seek_Help> list = Seek_Help.find("id = ?", seek_id).fetch();
-//		Seek_Help seek_Help = list.get(0);
-//		String seek_user = seek_Help.seek_user;
-//		String seek_content = seek_Help.seek_content;
 		/*获取该条信息的发起者，并且判断如果发起者和回复者不相同就将这条信息存到tips数据表里面*/
 		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		Date date = new Date();
@@ -53,6 +42,7 @@ public class ReSeek extends BaseCore {
 		tips.tip_from_id = seek_id;
 		dao.AddResources.addAsk_Tips(tips);
 		}
-		SeekHelp.seek_home(nowpage,(int)(seek_id/1));
+		SeekHelp.seek_home(nowpage,(int)(seek_id/1),0);
+	}
 	}
 }
--- app/controllers/SeekHelp.java
+++ app/controllers/SeekHelp.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import play.cache.Cache;
+import play.libs.Codec;
+import play.libs.Images;
+import play.libs.Images.Captcha;
 import play.mvc.Before;
 import models.*;
 public class SeekHelp extends BaseCore {
@@ -17,7 +21,7 @@ public class SeekHelp extends BaseCore {
 		}
 	}
 	/*返回寻求帮助信息的条目*/
-	public static void seek_home(int page,int seek_id){
+	public static void seek_home(int page,int seek_id,int imgstatue){
 		int allcount = (int)Seek_Help.count();
 		int allpage;
 		if(allcount % 9 == 0){
@@ -37,10 +41,16 @@ public class SeekHelp extends BaseCore {
 		/*分享的消息数目*/
 		List<Share_Tips> share_list = Share_Tips.find("tip_to_name = ? And tip_status = ?", session.get("user"),1).fetch();
 		size += share_list.size();
-		render(list,allpage,page,seek_id,size);
+		/*返回一个随机数，用来存储在服务器缓存中，对应相应的验证码的Id*/
+		String randomID = Codec.UUID();
+		render(list,allpage,page,seek_id,size,randomID,imgstatue);
 	}
 	/*增加寻求帮助信息*/
-	public static void addhelp(String comment_help,String seek_title){
+	public static void addhelp(String comment_help,String seek_title,String validateid,String code){
+		System.out.println(Cache.get(validateid));
+		if(!code.toUpperCase().equals(((String)Cache.get(validateid)).toUpperCase())){
+			seek_home(1,0,1);
+		}else{
 		 Seek_Help seek_Help = new Seek_Help();
 		 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
 		 Date date = new Date();
@@ -51,6 +61,16 @@ public class SeekHelp extends BaseCore {
 		seek_Help.seek_user = user;
 		seek_Help.seek_title = seek_title;
 		dao.AddResources.addSeek_Help(seek_Help);
-		seek_home(1,0);
+		seek_home(1,0,0);
+		}
+	}
+	/*返回验证码*/
+	public static void backValidate(String id){
+		Captcha captcha = Images.captcha(140,40);
+	    String code = captcha.getText("#ABCDEF");
+	    captcha.addNoise();
+        captcha.setBackground("#FFFFFF");
+	    Cache.set(id, code, "10mn");
+	    renderBinary(captcha);
 	}
 }
